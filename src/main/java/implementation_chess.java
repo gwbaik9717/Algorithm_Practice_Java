290050597831013c34d793fc2e044fcf9242b1b6
gwbaik9717 <gwbaik9717@icloud.com>
2022-01-13 10:46:56 +0900

    좌표값 구하기 수정

--- src/main/java/implementation_chess.java
+++ src/main/java/implementation_chess.java
@@ -2,29 +2,13 @@ import java.util.*;
 
 public class implementation_chess {
     public static void main(String[] args) {
-        char[] row = "123456789".toCharArray();
-        char[] column= "abcdefgh".toCharArray();
-        int[] dx = {-2, -2, -1, -1, 1, 1, 2, 2};
-        int[] dy = {-1, 1, -2, 2, -2, 2, -1, 1};
-
         Scanner sc = new Scanner(System.in);
         String location = sc.next();
+        int x = location.charAt(1) - '1';
+        int y = location.charAt(0) - 'a';
 
-        int x = 0;
-        int y = 0;
-
-        for(int i=0; i<row.length; i++){
-            if(row[i] == location.charAt(1)){
-                x=i;
-                break;
-            }
-        }
-        for(int i=0; i<row.length; i++){
-            if(column[i] == location.charAt(0)){
-                y=i;
-                break;
-            }
-        }
+        int[] dx = {-2, -2, -1, -1, 1, 1, 2, 2};
+        int[] dy = {-1, 1, -2, 2, -2, 2, -1, 1};
 
         int count = 0;
 
